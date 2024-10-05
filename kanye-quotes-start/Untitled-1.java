a9fae2796b2dc037f96afe8f68666d8b18cff769
Abiola Raji <araji191@mtroyal.ca>
2024-10-05 12:52:55 -0600

    add reference files

---
+++ kanye-quotes-start/Untitled-1.java
@@ -0,0 +1,77 @@
+//database.printArray();
+		
+		//arrayDatabase.printArray();
+		//CCDatabase hTDatabase = new HashTableDatabase();
+		//System.out.println(runTests(hTDatabase, filename));
+		//hTDatabase.printArray();
+		
+		/*
+		filename = "ops-20.txt";
+		database = new ArrayDatabase();
+		System.out.println(runTests(database, filename));
+		database = new HashTableDatabase();
+		System.out.println(runTests(database, filename));
+				
+		filename = "ops-200.txt";
+		database = new ArrayDatabase();
+		System.out.println(runTests(database, filename));
+		database = new HashTableDatabase();
+		System.out.println(runTests(database, filename));
+		
+		filename = "ops-2000.txt";
+		database = new ArrayDatabase();
+		System.out.println(runTests(database, filename));
+		database = new HashTableDatabase();
+		System.out.println(runTests(database, filename));
+		
+		filename = "ops-20000.txt";
+		database = new ArrayDatabase();
+		System.out.println(runTests(database, filename));
+		database = new HashTableDatabase();
+		System.out.println(runTests(database, filename));
+		
+		
+		filename = "ops-200000.txt";
+		database = new ArrayDatabase();
+		System.out.println(runTests(database, filename));
+		database = new HashTableDatabase();
+		System.out.println(runTests(database, filename));
+		*/
+		
+		
+		//CCDatabase database = new ArrayDatabase();
+		//database.printArray();
+
+
+
+        /*
+	public void printArray()
+	{
+		int count = 0;
+		for (int i = 0; i < accounts.length; i++)
+		{
+			//System.out.println(getAccount(accounts[i].getAccountNumber()));
+			count++;
+		}
+		System.out.println(count);
+	}	
+	*/
+
+    /*
+	public void printArray()
+	{
+		int count = 0;
+		for (int i = 0; i < accounts.length; i++)
+		{
+			if(accounts[i] != null)
+			{
+				if (accounts[i].isDeleted() == false)
+				{
+					System.out.println(getAccount(accounts[i].getAccountNumber()));
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}	
+	*/
