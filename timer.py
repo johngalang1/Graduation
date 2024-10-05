8cc573823f61a2f46ba1cfd7a84549b30ca6c3aa
Abiola Raji <araji191@mtroyal.ca>
2024-10-05 15:55:49 -0600

    changes

--- timer.py
+++ timer.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import pygame
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -80,6 +82,14 @@ def count_down(count):
             marks += f"{CHECK_MARK}"
         checkmark_label.config(text=marks)
 
+# ---------------------------- SOUND SETUP ---------------------------- #
+pygame.mixer.init()
+
+def play_sound(sound_file):
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load(sound_file)
+    pygame.mixer.music.play()
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
@@ -89,7 +99,7 @@ timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
 timer_label.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="download.png")
+tomato_img = PhotoImage(file="images/download.jpg")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
